57fc3348d7ae7f12ef51c846681c1de6220874e5
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-09 18:23:04 +0300

    Fix ssg

--- src/main.js
+++ src/main.js
@@ -1,53 +1,54 @@
 import "./assets/main.css";
 
-import itemsData from '@/data/items.json'
-import { ViteSSG } from 'vite-ssg'
+import itemsData from "@/data/items.json";
+import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
-import { routerOptions } from './router'
+import { routerOptions } from "./router";
 import i18n from "./i18n";
 import { initializeLanguage } from "@/util/lang.js";
 
 export const createApp = ViteSSG(
-    App, 
-    routerOptions,
-    ({app, router, initialState}) => {
-      initializeLanguage(router);
-      app.use(i18n);
-  
-      router.beforeEach((to, from, next) => {
-        const mergedQuery = { ...from.query, ...to.query };
-      
-        if (mergedQuery.resetfilters) {
-          if (to.query.resetfilters) {
-            delete mergedQuery.category;
-            delete mergedQuery.shop;
-            delete mergedQuery.search;
-          } else {
-            delete mergedQuery.resetfilters;
-          }
-        }
-      
-        if (mergedQuery.shop && mergedQuery.category) {
-          if (to.query.category) {
-            delete mergedQuery.shop;
-          }
-          else if (to.query.shop) {
-            delete mergedQuery.category;
-          }
-        }
-      
-        if (JSON.stringify(to.query) === JSON.stringify(mergedQuery)) {
-          next(); 
+  App,
+  routerOptions,
+  ({ app, router, initialState }) => {
+    initializeLanguage(router);
+    app.use(i18n);
+
+    router.beforeEach((to, from, next) => {
+      const mergedQuery = { ...from.query, ...to.query };
+
+      if (mergedQuery.resetfilters) {
+        if (to.query.resetfilters) {
+          delete mergedQuery.category;
+          delete mergedQuery.shop;
+          delete mergedQuery.search;
         } else {
-          next({ ...to, query: mergedQuery }); 
+          delete mergedQuery.resetfilters;
         }
-      });
-    }
+      }
+
+      if (mergedQuery.shop && mergedQuery.category) {
+        if (to.query.category) {
+          delete mergedQuery.shop;
+        } else if (to.query.shop) {
+          delete mergedQuery.category;
+        }
+      }
+
+      if (JSON.stringify(to.query) === JSON.stringify(mergedQuery)) {
+        next();
+      } else {
+        next({ ...to, query: mergedQuery });
+      }
+    });
+  },
+);
+
+export async function includedRoutes(paths, routes) {
+  const languages = ["en", "ru"];
+  const pages = languages.flatMap((lang) =>
+    itemsData.items.map((item) => `/${lang}/items/${item.id}`),
   );
-  
-  export async function includedRoutes(paths, routes) {
-    const languages = ['en', 'ru'];
-    return languages.flatMap(lang =>
-      itemsData.items.map(item => `/${lang}/items/${item.id}`)
-    );
-  }
+  const slimPreviews = itemsData.items.map((item) => `/slim/items/${item.id}`);
+  return [...pages, ...slimPreviews];
+}
